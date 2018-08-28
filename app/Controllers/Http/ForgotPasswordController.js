e345d50ca2dd8b5ffe18924462e4324956abfd1f
Thiago Maturana <thiagomatu@hotmail.com>
2018-08-28 20:11:24 -0300

    Adonis ES6 coding style

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -5,31 +5,31 @@ const crypto = require('crypto')
 const Mail = use('Mail')
 
 class ForgotPasswordController {
-    async store ({ request, response }) {
-        try {
-            const email = request.input('email')
+  async store ({ request, response }) {
+    try {
+      const email = request.input('email')
 
-            // if not find, Adonis returns an error with OrFail
-            const user = await User.findByOrFail('email', email)
+      // if not find, Adonis returns an error with OrFail
+      const user = await User.findByOrFail('email', email)
 
-            // token with 10 bytes and converted to hexadecimal
-            user.token = crypto.randomBytes(10).toString('hex')
-            user.token_created_at = new Date()
+      // token with 10 bytes and converted to hexadecimal
+      user.token = crypto.randomBytes(10).toString('hex')
+      user.token_created_at = new Date()
 
-            await user.save()
-            await Mail.send(
-                ['emails.forgot_password'], // template
-                { email, token: user.token, link: `${request.input('redirect_url')}?token=${user.token}` }, // variable for the template
-                message => {
-                    message
-                        .to(user.email)
-                        .from('thiago@teste.com.br', 'Thiago | Teste')
-                        .subject('Password Recovery')
-                })
-        } catch (err) {
-            return response.status(err.status).send({ error: { message: 'Something did not work. Do the e-mail exists?' } })
-        }
+      await user.save()
+      await Mail.send(
+        ['emails.forgot_password'], // template
+        { email, token: user.token, link: `${request.input('redirect_url')}?token=${user.token}` }, // variable for the template
+        message => {
+          message
+            .to(user.email)
+            .from('thiago@gonodeadonis.com.br', 'Thiago | GoNodeAdonis')
+            .subject('Password Recovery')
+        })
+    } catch (err) {
+      return response.status(err.status).send({ error: { message: 'Something did not work. Do the e-mail exists?' } })
     }
+  }
 }
 
 module.exports = ForgotPasswordController
