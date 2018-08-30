1c78284183105c28dbc7479a47a8c9b3c441b90b
Thiago Maturana de Oliveira <thiago.maturana@linx.com.br>
2018-08-30 18:34:45 -0300

    included the show method in FileController to show the file in image format, not in text and created a computed field in the File model to return the URL of the file

--- app/Controllers/Http/FileController.js
+++ app/Controllers/Http/FileController.js
@@ -6,6 +6,18 @@ const Helpers = use('Helpers')
  * Resourceful controller for interacting with files
  */
 class FileController {
+  async show ({ params, response }) {
+    try {
+      const file = await File.findOrFail(params.id)
+
+      // returning the file in image format
+      return response.download(Helpers.tmpPath(`uploads/${file.file}`))
+    } catch (err) {
+      return response
+        .status(err.status)
+        .send({ error: { message: 'Error when trying to show the file' } })
+    }
+  }
 
   /**
    * Create/save a new file.
@@ -14,7 +26,7 @@ class FileController {
   async store ({ request, response }) {
     try {
       // if there isn't a file with name file, do nothing
-      if (!request.file('file')) return;
+      if (!request.file('file')) return
 
       const upload = request.file('file', { size: '2mb' })
 
@@ -44,8 +56,6 @@ class FileController {
         .send({ error: { message: 'Error on uploading the file' } })
     }
   }
-
-
 }
 
 module.exports = FileController
--- app/Models/File.js
+++ app/Models/File.js
@@ -1,8 +1,17 @@
 'use strict'
 
 const Model = use('Model')
+const Env = use('Env')
 
 class File extends Model {
+  // virtual field (computed) that does not exists in the table
+  static get computed () {
+    return ['url']
+  }
+  // everytime we need to show a file in the fron-end, we put the url together in the return
+  getUrl ({ id }) {
+    return `${Env.get('APP_URL')}/files/${id}`
+  }
 }
 
 module.exports = File
