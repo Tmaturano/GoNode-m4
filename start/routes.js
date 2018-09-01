0b2c2a0ff35d69d08addc799c1dfa67cd66c8d16
Thiago Maturana <thiagomatu@hotmail.com>
2018-09-01 10:15:23 -0300

    created the CRUD for Projects

--- start/routes.js
+++ start/routes.js
@@ -21,5 +21,13 @@ Route.post('sessions', 'SessionController.store')
 Route.post('passwords', 'ForgotPasswordController.store')
 Route.put('passwords', 'ForgotPasswordController.update')
 
-Route.post('files', 'FileController.store')
 Route.get('files/:id', 'FileController.show')
+
+/**
+ * User will only be able to call these routes if send an auth token
+ */
+Route.group(() => {
+  Route.post('files', 'FileController.store')
+
+  Route.resource('projects', 'ProjectController').apiOnly()
+}).middleware(['auth'])
