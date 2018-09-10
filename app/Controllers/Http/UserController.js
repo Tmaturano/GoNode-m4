5371e4fb3fba86e0f8a8ed2c895f66a9f352509b
Thiago Maturana <thiagomatu@hotmail.com>
2018-09-10 11:19:06 -0300

    included the Database Transaction from Adonis to only commit when both user and user address are inserted in the database

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,17 +1,22 @@
 'use strict'
 
+const Database = use('Database')
 const User = use('App/Models/User')
 
 class UserController {
   async store ({ request }) {
     const data = request.only(['username', 'email', 'password'])
 
+    const transaction = await Database.beginTransaction()
+
     // input returns only 1 information from the body of the request
     const addresses = request.input('addresses')
 
-    const user = await User.create(data)
+    const user = await User.create(data, transaction)
 
-    await user.addresses().createMany(addresses)
+    await user.addresses().createMany(addresses, transaction)
+
+    await transaction.commit()
 
     return user
   }
